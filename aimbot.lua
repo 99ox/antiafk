45883353baa829a66625526f95bb0ad545e25a52
9itar <96202805+99ox@users.noreply.github.com>
2024-07-19 19:53:33 +0100

    Update and rename aimbotv2 to aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -220,14 +220,14 @@ end
 function Environment.Functions:ResetSettings()
     Environment.Settings = {
         Enabled = true,
-        TeamCheck = false,
+        TeamCheck = true,
         AliveCheck = true,
         WallCheck = false,
         Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
         ThirdPerson = false, -- Uses mousemoverel instead of CFrame to support locking in third person (could be choppy)
         ThirdPersonSensitivity = 3, -- Boundary: 0.1 - 5
         TriggerKey = "LeftShift", -- Change trigger key to Left Shift
-        Toggle = false,
+        Toggle = true,
         LockPart = "Head" -- Body part to lock on
     }
 
